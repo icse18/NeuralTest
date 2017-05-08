5f60019f7ceaad0020cd79e02199ed5407489b3a
Joel <joel_ong@mymail.sutd.edu.sg>
2017-05-08 23:29:14 +0700

    updated generator

--- generator/synthetic_data_generator.py
+++ generator/synthetic_data_generator.py
@@ -1,14 +1,9 @@
 # Generating synthetic data for Neural Network Trianing
 import numpy as np
-
+import sys
 from predicates import predicate_1
 
-"""
-NOTE: Adjust the digits parameters of the lambda function f and g to 
-      balance the number of True / False data generated.
-"""
-
-def data_generator(num_data, digits):
+def data_generator_numpy(num_data):
     print("Generating data...")
     current_data = 0
     seen = set()
@@ -16,12 +11,8 @@ def data_generator(num_data, digits):
     labels = []
     num_true = 0
     while(current_data < num_data):
-        f = lambda: int(''.join(np.random.choice(list("0123456789"))
-                        for i in range(np.random.randint(1, digits + 1))))
-        g = lambda: int(''.join(np.random.choice(list("0123456789"))
-                        for i in range(np.random.randint(1, digits + 3))))
-        
-        x, y, z = f(), f(), g()
+        sample = np.random.randint(-sys.maxsize - 1, sys.maxsize, 3)
+        x, y, z = sample[0], sample[1], sample[2]
         key = tuple(sorted((x,y)))
         if key in seen:
             continue
@@ -29,21 +20,25 @@ def data_generator(num_data, digits):
         seen.add(key)
         vectors.append(tuple((x,y,z)))
         result = predicate_1(x,y,z)
-        if(result): 
+        if (num_true > current_data - num_true):
+            continue
+        elif(result): 
             labels.append(1)
             num_true += 1
         else:
             labels.append(0)
-    print("Data generation completed...")
+
     print("Writing to file...")
-    f = open("vectors_50k_6d.txt", "w+")
+    f = open("vectors.txt", "w+")
     f.write("\n".join("%s %s %s" % value for value in vectors))
     f.close()
     
-    f = open("labels_50k_6d.txt", "w+")
+    f = open("labels.txt", "w+")
     f.write("\n".join("%s" % label for label in labels))
     f.close()
     
     print("Number of True labels: %s" % num_true)
     print("Number of False labels: %s" % (num_data - num_true))
     return(vectors, labels)
+
+data_generator_numpy(30000)
